81798e90a3e3dd5f975da91cb6bdaaa5e1349e88
Xuejian Ma <Xuejian.Ma@gmail.com>
2020-11-29 17:35:39 -0600

    update

--- DiffusionAnalysis/main.py
+++ DiffusionAnalysis/main.py
@@ -8,11 +8,13 @@ from PyQt5.QtCore import QFile, Qt
 # from PyQt5.QtUiTools import QUiLoader
 from PyQt5.QtGui import QPixmap,QImage, QPainter, QPen, QBrush, QPolygon
 import pyqtgraph as pg
-from utils import readSimulatedImReCSV,readImRePhase
+from utils import readSimulatedImReCSV,readImRePhase,diffusion_map,radialAverageByLinecuts
+from PIL import Image
 import random
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.figure import Figure
+
 
 class gui(QWidget):
     def __init__(self):
@@ -24,12 +26,15 @@ class gui(QWidget):
         self.piximg = None
         self.directoryName = None
         self.responseFile = None
-        self.Widget_response.setBackground('w')
+        self.widget_response.setBackground('w')
+        self.widget_laserScreenshot.setBackground('w')
+        self.widget_laserFit.setBackground('w')
         self.im_sim_raw = None
         self.re_sim_raw = None
         self.cond_array = None
         self.im_sim = None
         self.re_sim = None
+        self.laserScreenshot = None
 
         # self.graphWidget = pg.PlotWidget()
         # # self.setCentralWidget(self.graphWidget)
@@ -39,7 +44,7 @@ class gui(QWidget):
         #
         # # plot data: x, y values
         # self.graphWidget.plot(hour, temperature)
-        # self.Widget_response = self.graphWidget
+        # self.widget_response = self.graphWidget
 
 
     def load_ui(self):
@@ -48,19 +53,19 @@ class gui(QWidget):
 
 
     # def extraConfiguration(self):
-    #     self.Widget_response.canvas = FigureCanvas(Figure())
-    #     # self.Widget_response.canvas.resize(2,1)
+    #     self.widget_response.canvas = FigureCanvas(Figure())
+    #     # self.widget_response.canvas.resize(2,1)
     #     vertical_layout = QVBoxLayout()
-    #     vertical_layout.addWidget(self.Widget_response.canvas)
+    #     vertical_layout.addWidget(self.widget_response.canvas)
     #
     #
     #
-    #     self.Widget_response.canvas.axes = self.Widget_response.canvas.figure.add_subplot(111)
-    #     self.Widget_response.canvas.axes.set_xlabel('MIM-Im')
-    #     self.Widget_response.canvas.axes.set_ylabel('MIM-Re')
-    #     # self.Widget_response.canvas.figure.tight_layout(pad=5)
-    #     # self.Widget_response.canvas.axes.set_size(1,2)
-    #     self.Widget_response.setLayout(vertical_layout)
+    #     self.widget_response.canvas.axes = self.widget_response.canvas.figure.add_subplot(111)
+    #     self.widget_response.canvas.axes.set_xlabel('MIM-Im')
+    #     self.widget_response.canvas.axes.set_ylabel('MIM-Re')
+    #     # self.widget_response.canvas.figure.tight_layout(pad=5)
+    #     # self.widget_response.canvas.axes.set_size(1,2)
+    #     self.widget_response.setLayout(vertical_layout)
     #
     #     # self.label_ylabel.rotate(90)
 
@@ -73,6 +78,9 @@ class gui(QWidget):
         # self.pushButton_draw.clicked.connect(self.update_graph)
         # self.pushButton_scatter.clicked.connect(self.plotResponse)
         self.pushButton_plotResponse.clicked.connect(self.plotResponse)
+        self.pushButton_laserScreenshot.clicked.connect(self.selectLaserScreenshot)
+        self.pushButton_laserFit.clicked.connect(self.plotLaserFit)
+        self.pushButton_laserFitClear.clicked.connect(self.widget_laserFit.clear)
 
     def selectDirectory(self):
         self.directoryName = QFileDialog.getExistingDirectory(self, 'Select directory')#getOpenFileName(self, 'Open file', '.', '')
@@ -116,7 +124,7 @@ class gui(QWidget):
 
     # def plotScatters(self):
 
-        # self.Widget_response.plot(im_all, re_all, pen=None,symbol = 'o',symbolPen = pg.mkPen(color=(0, 0, 255), width=0),symbolSize=1)
+        # self.widget_response.plot(im_all, re_all, pen=None,symbol = 'o',symbolPen = pg.mkPen(color=(0, 0, 255), width=0),symbolSize=1)
 
     def selectResponse(self):
         self.responseFile = QFileDialog.getOpenFileName(self, 'Select response curve',)[0]
@@ -131,35 +139,59 @@ class gui(QWidget):
         self.plotResponse()
 
     # def updateResponse(self,x,y):
-    #     # self.Widget_response.canvas.axes.clear()
-    #     # self.Widget_response.canvas.axes.plot(x,y)
-    #     # # self.Widget_response.canvas.axes.set_xlabel('MIM-Im')
-    #     # # self.Widget_response.canvas.axes.set_ylabel('MIM-Re')
-    #     # # self.Widget_response.canvas.figure.tight_layout(pad=3)
-    #     # # self.Widget_response.canvas.axes.legend(('cosinus', 'sinus'), loc='upper right')
-    #     # # self.Widget_response.canvas.axes.set_title(' Cosinus - Sinus Signal')
+    #     # self.widget_response.canvas.axes.clear()
+    #     # self.widget_response.canvas.axes.plot(x,y)
+    #     # # self.widget_response.canvas.axes.set_xlabel('MIM-Im')
+    #     # # self.widget_response.canvas.axes.set_ylabel('MIM-Re')
+    #     # # self.widget_response.canvas.figure.tight_layout(pad=3)
+    #     # # self.widget_response.canvas.axes.legend(('cosinus', 'sinus'), loc='upper right')
+    #     # # self.widget_response.canvas.axes.set_title(' Cosinus - Sinus Signal')
     #     #
-    #     # self.Widget_response.canvas.draw()
-    #     self.Widget_response.clear()
-    #     self.Widget_response.plot(x,y)
+    #     # self.widget_response.canvas.draw()
+    #     self.widget_response.clear()
+    #     self.widget_response.plot(x,y)
 
     def plotResponse(self):
-        self.Widget_response.clear()
+        self.widget_response.clear()
 
         path = self.lineEdit_directory.text()
         sub_str = self.lineEdit_subdirectories.text()
         sub_list = sub_str.split(',')
         folderPathList = [path + '/' + item + '/' for item in sub_list]
         im_all, re_all = readImRePhase(folderPathList, fileNameIm=self.lineEdit_im.text(), fileNameRe=self.lineEdit_re.text(), naiveScale=float(self.lineEdit_unitConversionData.text()))
-        self.Widget_response.plot(im_all, re_all,pen=pg.mkPen('b', width=1))
+        self.widget_response.plot(im_all, re_all,pen=pg.mkPen('b', width=1))
 
         self.cond_array, self.im_sim_raw, self.re_sim_raw = readSimulatedImReCSV(self.lineEdit_response.text(), comsolScale=float(self.lineEdit_unitConversionSimulation.text()))
         self.im_sim = self.im_sim_raw*float(self.lineEdit_xrefactor.text())
         self.re_sim = self.re_sim_raw*float(self.lineEdit_yrefactor.text())
-        self.Widget_response.plot(self.im_sim, self.re_sim)
-        print(12234)
+        self.widget_response.plot(self.im_sim, self.re_sim)
+        # print(12234)
 
+    def selectLaserScreenshot(self):
+        self.laserScreenshot = QFileDialog.getOpenFileName(self, 'Select Laser Screenshot Image', )[0]
+        self.lineEdit_laserScreenshot.setText(self.laserScreenshot)
+        pixmap = QPixmap(self.lineEdit_laserScreenshot.text()).scaled(200, 200, Qt.KeepAspectRatio)
+        self.label_laserScreenshot.setPixmap(pixmap)
+        # print(self.lineEdit_laserScreenshot.text())
+        # image_array = pg.QtGui.QGraphicsPixmapItem(pg.QtGui.QPixmap(self.lineEdit_laserScreenshot.text()))#np.asarray(Image.open(self.lineEdit_laserScreenshot.text()))
+        # print(image_array)
+        # self.widget_laserScreenshot.addItem(image_array)
 
+        # self.piximg = pixtmp.scaled(200, 200, Qt.KeepAspectRatio)
+
+    def plotLaserFit(self):
+        xx,yy,z = diffusion_map(1e-100,float(self.lineEdit_laserRadius.text())*np.sqrt(2),point_num=100,pos_max=15)
+        x_axis_fit = xx[0]
+        total = np.sum(z)
+        z /= total
+        z_axis_fit = z[round(z.shape[0] / 2)]
+        z0 = np.max(z_axis_fit)  # or np.max(z). They are the same
+        # plt.plot(x_axis_fit, z_axis_fit / z0, label='L={}μm'.format(int(L)), color='black')
+        self.widget_laserFit.plot(x_axis_fit,z_axis_fit/z0)
+
+        laser_rList, laser_zList, laser_rDict = radialAverageByLinecuts(laserimg_cropped, (0, 0), laser_X_cropped,
+                                                                        laser_Y_cropped, radialSteps=300,
+                                                                        threshold=1 / 6 * 1, angleSteps=10)
 
 
 if __name__ == "__main__":
